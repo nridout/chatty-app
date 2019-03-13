8fb1af7bce59d830132a6547ac9133b657a5751d
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-13 10:47:14 -0400

    Added console log to monito render process

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -3,6 +3,7 @@ import Message from './Message.jsx';
 
 class MessageList extends Component {
   render() {
+    console.log('Rendering <Message List />')
     const messages = this.props.messages.map(message => (
       <Message key={message.id} message={message} />
     ));
