73102da516035e7b5ba32a58c1e9ecfd5f042b67
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-13 10:46:38 -0400

    Added on change and keypress to capture input values

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,22 +1,71 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
+// Chatbar conponent renders Username and Content input fields
+// Username value defaults to "Username" prop from app
+// Username input and Content input fields accept values on enter,
+// Reset state to trigger render, and pass values to the App Component
 class ChatBar extends Component {
+
+  constructor(props) {
+    super(props)
+    // set the initial state of the username & content as null
+    this.state = {
+      username: '',
+      content: ''
+    }
+  }
+
   render() {
+    // Console Log render of Chatbar component
+    console.log('Rendering <Chatbar />')
+    console.log(this.state)
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.currentUser} />
-        <ChatbarMessage />
+        <input className="chatbar-username"
+          value={this.state.username}
+          placeholder={this.props.currentUser}
+          onChange={this._handleUsernameChange}
+          onKeyPress={this._handleUsernameKeyPress}
+        />
+        <input className="chatbar-message"
+          value={this.state.content}
+          placeholder="Type a message and hit ENTER"
+          onChange={this._handleContentChange}
+          onKeyPress={this._handleContentKeyPress}
+         />
       </footer>
-    );
+    )
   }
-}
 
-function ChatbarMessage() {
-    return (
-      <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-    );
-}
+  // Handle username change event by updating the state
+  _handleUsernameChange = (event) => {
+    this.setState({ username: event.target.value})
+  }
 
+  // Handle username submission on Enter
+  // Send the username to app & clear input value
+  _handleUsernameKeyPress = (event) => {
+    if (event.key == 'Enter') {
+      console.log('enter press for Username! ')
+      this.setState({ username: '' })
+    }
+  }
+
+  // Handle new message content by updating the state
+  _handleContentChange = (event) => {
+    this.setState({ content: event.target.value })
+  }
+
+  // Handle new message submission on Enter
+  // Send the username to app & clear input value
+  _handleContentKeyPress = (event) => {
+    if (event.key == 'Enter') {
+      console.log('enter press for Content! ')
+      this.setState({ content: '' })
+    }
+  }
+
+}
 
 export default ChatBar;
 
