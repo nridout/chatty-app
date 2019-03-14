acb9e0c7f2c567aa8799a8a0872b4e2a2a4b4195
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-14 12:49:53 -0400

    updated classes to render message v. notification

--- src/Message.jsx
+++ src/Message.jsx
@@ -4,14 +4,12 @@ class Message extends Component {
   render() {
     console.log('Rendering <Message />')
     return (
-      <div className="message">
-        <span className="message-username">{this.props.message.username}</span>
-        <span className="message-content">{this.props.message.content}</span>
+      <div className={this.props.message.type}>
+        <span className={this.props.message.type + '-username'}>{this.props.message.username}</span>
+        <span className={this.props.message.type + '-content'}>{this.props.message.content}</span>
       </div>
     );
   }
 }
 
 export default Message;
-
-
