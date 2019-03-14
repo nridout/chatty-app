5761bbfcb5208d7ae7e3f92e377362d018c17fab
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-14 12:48:55 -0400

    added incoming message handling & update username

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,24 @@ class App extends Component {
       this.socket.addEventListener('open', (event) => {
         console.log('Connected to server')
       })
-      // Listen for incoming messages
+      // Listen for incoming messages & notifications
       this.socket.addEventListener('message', (event) => {
-        console.log('Simulating incoming message', event.data);
-        // Add new incoming messages to the list of messages in the data store
-        const newMessage = JSON.parse(event.data)
-        const messages = this.state.messages.concat(newMessage)
-        // Update the state of the app component
-        this.setState({ messages: messages })
+        console.log('Incoming data:', event.data);
+        const data = JSON.parse(event.data)
+        // Handle incoming message types
+        switch(data.type) {
+          case 'incomingMessage':
+            data.type = 'message'
+            this._updateMessageList(data)
+          break
+          case 'incomingNotification':
+            data.type = 'notification'
+            this._updateMessageList(data)
+          break
+          default:
+          // Else, show error in console
+          throw new Error('Unknown event type' + data.type)
+        }
       })
     }, 3000);
   }
@@ -44,21 +54,41 @@ class App extends Component {
         <a href="/" className="navbar-brand">Chatty</a>
       </nav>
         <MessageList messages={this.state.messages} />
-        <ChatBar currentUser={this.state.currentUser.name} addMessage={this._addMessage} />
+        <ChatBar currentUser={this.state.currentUser.name} addMessage={this._addMessage} updateUsername={this._updateUsername} />
       </div>
     );
   }
 
-  // Creates a new message object & sends to server
-  // Resets state of username to submitted username
+  // Create a postmessage object & send to server
   _addMessage = (username, content) => {
     const newMessage = {
-      type: 'incomingMessage',
+      type: 'postMessage',
       username: username,
       content: content,
     }
+    console.log('New message sent to server:', newMessage)
     this.socket.send(JSON.stringify(newMessage))
-    this.setState({currentUser: { name: username } })
+  }
+
+  // If the user changes their username, send a postNotification to the server
+  // Update the currentUser.name
+  _updateUsername = (username) => {
+    if (username !== this.state.currentUser.name) {
+      const newNotification = {
+        type: 'postNotification',
+        content: `${this.state.currentUser.name} has changed their name to ${username}`
+      }
+      console.log('New notification sent to server:', newNotification)
+      this.socket.send(JSON.stringify(newNotification))
+      this.setState({ currentUser: { name: username } })
+    }
+  }
+
+  // Add new incoming messages to the list of messages in the data store
+  _updateMessageList = (message) => {
+    const messages = this.state.messages.concat(message)
+    // Update the state of the app component
+    this.setState({ messages: messages })
   }
 
 }
