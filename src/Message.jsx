cda9605b7b093741cd48b98af07fffbeaa7a439f
Nicole Ridout <nicoleridout@Nicoles-MacBook-Pro.local>
2019-03-14 14:16:50 -0400

    added note about username div

--- src/Message.jsx
+++ src/Message.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class Message extends Component {
   render() {
     console.log('Rendering <Message />')
+    // *** Think about how to "turn off" the username div when its a system message
     return (
       <div className={this.props.message.type}>
         <span className={this.props.message.type + '-username'}>{this.props.message.username}</span>
